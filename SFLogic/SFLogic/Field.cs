ef543f02e34429167f41f37bb1f5977246c7188b
Sergey Romanchuk <wsx1105@mail.ru>
2015-10-13 17:28:55 +0300

    Second commit

---
+++ SFLogic/SFLogic/Field.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SFLogic
+{
+    /// <summary>
+    /// Игровое поле
+    /// Хранит информацию об игровом поле
+    /// </summary>
+    public class Field
+    {
+        public List<List<Cell>> matrix;
+        public Size size;
+
+        /// <summary>
+        /// Создание игрового поля
+        /// </summary>
+        /// <param name="matrix">Матрица ячеек (Cell)</param>
+        public Field(List<List<Cell>> matrix)
+        {
+            if (this.matrixValidation(matrix)) // Проверяем валидность матрицы
+            {
+                this.matrix = matrix;
+                this.size.Height = matrix.Count;
+                this.size.Width = matrix[0].Count;
+            }
+            else 
+            {
+                throw new SeaFightException("Field: Incorrectly transmitted field. Height and Width must be equal");
+            }
+        }
+
+        /// <summary>
+        /// Проверяет валидность матрицы игрового поля
+        /// </summary>
+        /// <param name="matrix">Матрица игрового поля</param>
+        /// <returns></returns>
+        private bool matrixValidation(List<List<Cell>> matrix)
+        {
+            if (matrix.Count == matrix[0].Count)
+                return true;
+            return false;
+        }
+    }
+}
---
+++ SFLogic/SFLogic/ShipStatus.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SFLogic
+{
+    /// <summary>
+    /// Статусы корабля (например, живой, убит, ранен...)
+    /// </summary>
+    public enum ShipStatus
+    {
+        alive, // живой
+        dead,  // убит
+        injured // ранен
+    }
+}
